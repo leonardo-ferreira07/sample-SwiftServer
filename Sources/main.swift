1d2612350d0871c5e307098f62204fef3fc2a42a
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-09-08 22:35:57 +0800

    Added SwiftyJSON and another endpoint

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,10 +1,35 @@
 import Kitura
+import SwiftyJSON
+
+#if os(Linux)
+    import Glibc
+srand(UInt32(time(nil)))
+#else
+    import Darwin.C
+#endif
 
 let router = Router()
 
 router.get("/") {
     request, response, next in
     response.send("Hello World, Leonardo")
+    next()
+}
+
+let salutations: [String] = [
+    "How are you today?",
+    "Where do you live?",
+    "What's your job?"
+]
+
+router.get("/questions") {
+    request, response, next in
+    #if os(Linux)
+        let idx = Int(random() % salutations.count)
+    #else
+        let idx = Int(arc4random_uniform(UInt32(salutations.count)))
+    #endif
+    response.send(json: JSON(["text": salutations[idx]]))
     next()
 }
 
